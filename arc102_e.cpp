be0e2d67967613b0a9130b9ab073d5d3f9fd2ebf
hungp2812 <hung05.pham@gmail.com>
2021-10-21 23:09:05 +0700

    New solution 21-10-2021

--- arc102_e.cpp
+++ arc102_e.cpp
@@ -1,154 +1,147 @@
 /*
- /+===================================================+\
-//+---------------------------------------------------+\
-|.|\...>>>>>>> Hollwo_Pelw's 2nd template<<<<<<<...//|.|
-\+---------------------------------------------------+//
- \+===================================================+/
+// is short or still long ???
+hollwo_pelw's template(short)
+// Note : -Dhollwo_pelw_local
 */
-#include <bits/stdc++.h>
-using namespace std;
-// type
-typedef long long ll;
-typedef long double ld;
-// pair
-#define F                   first
-#define S                   second
-#define pii                 pair<int, int>
-#define pll                 pair<ll, ll>
-#define pdd                 pair<ld, ld>
-// vector & !!?(string)
-#define eb                  emplace_back
-#define pb                  push_back
-#define all(a)              a.begin(), a.end()
-#define rall(a)             a.rbegin(), a.rend()
-#define sz(a)               a.size()
-// I/O
-#define setpre(n)           fixed << setprecision(n)
-#define Ptest(x)            return cout << x << endl, (void) 0;
-bool endline = false;
-template<class T>
-istream& operator >> (istream& inp, vector<T>& v){
-    for (auto& it : v) inp >> it;
-    return inp;
-}
-template<class T>
-ostream& operator << (ostream& out, vector<T>& v){
-    for (auto& it : v) out << it << (endline ? "\n" : " ");
-    return out;
-}
-template<class T, class U>
-istream& operator >> (istream& inp, pair<T, U>& v){
-    inp >> v.F >> v.S;
-    return inp;
-}
-template<class T, class U>
-ostream& operator << (ostream& out, pair<T, U>& v){
-    out << v.F << ' ' << v.S;
-    return out;
-}
-void debug(){
-    cout << endl;
-}
-template <typename H, typename... T>
-void debug(H a, T... b){
-    cout << a << ' ';
-    debug(b...);
-}
-// geometry calculate
-#define pi                  acos(-1.0)
-#define g_sin(a)            sin(a*pi/180)
-#define g_cos(a)            cos(a*pi/180)
-#define g_tan(a)            tan(a*pi/180)
-// set val
-#define ms0(a)              memset(a,        0, sizeof(a));
-#define ms1(a)              memset(a,        1, sizeof(a));
-#define msn1(a)             memset(a,       -1, sizeof(a));
-#define msinf(a)            memset(a, 0x3f3f3f, sizeof(a));
 
-void FAST_IO(string filein = "", string fileout = ""){
-    if (fopen(filein.c_str(), "r")){
-        freopen(filein.c_str(), "r", stdin);
-        freopen(fileout.c_str(), "w", stdout);
-    }
-    cin.tie(0), cout.tie(0) -> sync_with_stdio(0);
+#include <bits/stdc++.h>
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/trie_policy.hpp>
+// #include <ext/rope>
+
+using namespace std;
+// using namespace __gnu_pbds;
+// using namespace __gnu_cxx;
+
+void FAST_IO(string filein = "", string fileout = "", string fileerr = ""){
+	if (fopen(filein.c_str(), "r")){
+		freopen(filein.c_str(), "r", stdin);
+		freopen(fileout.c_str(), "w", stdout);
+#ifdef hollwo_pelw_local
+		freopen(fileerr.c_str(), "w", stderr);
+#endif
+	}
+	cin.tie(0), cout.tie(0) -> sync_with_stdio(0);
 }
 
 void Hollwo_Pelw();
 
 signed main(){
-    #ifdef hollwo_pelw_local
-        FAST_IO("input.inp", "output.out");
-        auto start = chrono::steady_clock::now();
-    #else
-        FAST_IO(".inp", ".out");
-    #endif
-    int testcases = 1;
-    // cin >> testcases;
-    for (int test = 1; test <= testcases; test++){
-        // cout << "Case #" << test << ": ";
-        Hollwo_Pelw();
-    }
-    #ifdef hollwo_pelw_local
-        auto end = chrono::steady_clock::now();
-        cout << endl;
-        cout << "Excution time : " << chrono::duration_cast<chrono::milliseconds> (end - start).count() << "[ms]" << endl;
-    #endif
-    return 0;
+#ifdef hollwo_pelw_local
+	FAST_IO("input.inp", "output.out", "error.err");
+	auto start = chrono::steady_clock::now();
+#else
+	FAST_IO("hollwo_pelw.inp", "hollwo_pelw.out");
+#endif
+	int testcases = 1;
+	// cin >> testcases;
+	for (int test = 1; test <= testcases; test++){
+		// cout << "Case #" << test << ": ";
+		Hollwo_Pelw();
+	}
+#ifdef hollwo_pelw_local
+	auto end = chrono::steady_clock::now();
+	cout << "\nExcution time : " << chrono::duration_cast<chrono::milliseconds> (end - start).count() << "[ms]" << endl;
+#endif
+	return 0;
 }
 
-// constant
-const int allmod[3] = {(int) 1e9 + 7, 998244353, (int) 1e9 + 9};
-const int mod = allmod[1];
-const int MAXN = 1e5 + 5;
-const int inf = 2e9;
-const ll linf = 1e18;
-#define int long long
+const int N = 4000 + 5, mod = 998244353;
 
-int n, k, C[4040][4040];
+#define defop(type, op) \
+	friend inline type operator op (type a, const type &b) { return a op##= b; } \
+	type& operator op##= (const type &oth)
+ 
+template<class T> struct mod_int {
+	static constexpr int mod() { return T::mod; }
+ 
+	int v;
+	mod_int(long long _v = 0) 
+		: v(norm(_v)) {}
+	
+	inline int norm(long long a) {
+		return a < 0 ? a % mod() + mod() : a % mod();
+	}
+	mod_int neg() const { return v == 0 ? 0 : mod() - v; }
+	
+	template <typename U>
+	explicit operator U() const { return v; }
+ 
+	mod_int operator - () const { return neg(); }
+	mod_int operator + () const { return mod_int(*this); }
+ 
+	mod_int& operator -- () { if (v == 0) v = mod(); -- v; return *this; }
+	friend mod_int operator -- (mod_int& a, int) { mod_int r = a; --a; return r; }
+	mod_int& operator ++ () { ++ v; if (v == mod()) v = 0; return *this; }
+	friend mod_int operator ++ (mod_int& a, int) { mod_int r = a; ++a; return r; }
+	
+	defop(mod_int, +) {
+		if ((v += oth.v) >= mod()) v -= mod();
+		return *this;
+	}
+	defop(mod_int, -) {
+		if ((v -= oth.v) < 0) v += mod();
+		return *this;
+	}
+	defop(mod_int, *) {
+		return *this = norm(1ll * v * oth.v);
+	}
+ 
+	mod_int pow(long long b) const {
+		mod_int a = *this, r = 1;
+		while (b) {
+			if (b&1) r *= a;
+			b >>= 1; a *= a;
+		}
+		return r;
+	}
+	mod_int inv() const { return pow(mod() - 2); }
+ 
+	defop(mod_int, /) { 
+		return *this *= oth.inv();
+	}
+ 
+#define compare(op) friend bool operator op \
+(const mod_int& lhs, const mod_int& rhs) { return lhs.v op rhs.v; }
+	compare(==) compare(!=) compare(<) compare(<=) compare(>) compare(>=)
+#undef compare
+ 
+	friend std::ostream& operator << (std::ostream& out, const mod_int& n) { return out << int(n); }
+	friend std::istream& operator >> (std::istream& inp, mod_int& n) { long long x; inp >> x; n = mod_int(x); return inp; }
+};
+ 
+struct modular { static constexpr int mod = static_cast<int>(998244353); };
+ 
+// struct modular { static int mod; };
+// int modular::mod;
+ 
+using Mint = mod_int<modular>;
+
+Mint C[N][N];
+int n, k;
 
 void solve(int p){
-    int ans = 0;
-    // cout << p << " ";
-    for (int i = 0; i <= p; i++){
-        // choose exactly i pairs
-        if (n >= 2 * i && k + n >= 2 * i + 1)
-            ans += C[p][i] * C[k + n - 2 * i - 1][n - 2 * i] % mod * (i & 1 ? -1 : 1);
-    }
-    ans = ((ans % mod) + mod) % mod;
-    cout << ans << "\n";
+	Mint ans = 0;
+	for (int i = 0; i <= p; i++){
+		// choose exactly i pairs
+		if (n >= 2 * i && k + n >= 2 * i + 1)
+			ans += C[p][i] * C[k + n - 2 * i - 1][n - 2 * i] * (i & 1 ? -1 : 1);
+	}
+	cout << ans << "\n";
 }
 
 void Hollwo_Pelw() {
-    C[0][0] = 1;
-    for (int i = 1; i < 4040; i++){
-        C[i][0] = C[i][i] = 1;
-        for (int j = 1; j < i; j++){
-            C[i][j] = (C[i - 1][j - 1] + C[i - 1][j]) % mod;
-        }
-    }
-    cin >> k >> n;
-    for (int i = 2; i <= 2 * k; i++){
-        int p = i;
-        if (p > k + 1)
-            p = 2 * k + 2 - p;
-        solve(p / 2); // total p / 2 pairs that have sum = i
-    }
+	C[0][0] = 1;
+	for (int i = 1; i < N; i++){
+		C[i][0] = C[i][i] = 1;
+		for (int j = 1; j < i; j++)
+			C[i][j] = C[i - 1][j - 1] + C[i - 1][j];
+	}
+	cin >> k >> n;
+	for (int i = 2; i <= 2 * k; i++){
+		int p = i;
+		if (p > k + 1)
+			p = 2 * k + 2 - p;
+		solve(p / 2); // total p / 2 pairs that have sum = i
+	}
 }
-
-/*
-
-./-=====>>><<<-------- DEBUG -------->>><<<=====-\.
-/.................................................\
-+====================== INP ======================+
-
-
-
-+====================== OUT ======================+
-
-
-
-+======================*****======================+
-\................................................./
-.\-=====>>><<<--------= END =-------->>><<<=====-/.
-
-*/
